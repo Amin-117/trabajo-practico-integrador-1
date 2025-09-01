7f52c9c8a6f61b325b983f3de898faed6278cb86
Amin_117 <garridoamin@gmail.com>
2025-09-01 14:29:30 -0300

    añadido de carpetas y archivos

---
+++ src/models/user.model.js
@@ -0,0 +1,42 @@
+import { DataTypes, ENUM } from "sequelize";
+
+const userModel = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM("admin", "user"),
+    allowNull: false,
+    defaultValue: "user"
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+  deleted_at: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+});
+
+export default userModel;
